9ce37b1f721495167e5adf509b5b9fb984c3120c
Yari Andres <yari.andres@gmail.com>
2019-10-02 19:19:24 +0200

    changes to the algorithm file

--- algorithms.js
+++ algorithms.js
@@ -11,18 +11,25 @@
 // }
 
 // Consider an array of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
-function countSheeps(arrayOfSheep) {
-  const result = arrayOfSheep.filter(i => i === true).length;
-  console.log('number of the found elements: ' + result);
+// function countSheeps(arrayOfSheep) {
+//   const result = arrayOfSheep.filter(i => i === true).length;
+//   console.log('number of the found elements: ' + result);
+// }
+
+// var arr = [
+//   true, true, true, false,
+//   true, true, true, true,
+//   true, false, true, false,
+//   true, false, false, true,
+//   true, true, true, true,
+//   false, false, true, true
+// ]
+
+// countSheeps(arr);
+
+function describeAge(age) {
+  return (age <= 12 ? "You're a(n) kid" : "You're a(n) teenager")
+  console.log(age)
 }
+describeAge(12)
 
-var arr = [
-  true, true, true, false,
-  true, true, true, true,
-  true, false, true, false,
-  true, false, false, true,
-  true, true, true, true,
-  false, false, true, true
-]
-
-countSheeps(arr);

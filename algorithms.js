f6277d72957eb4ff7ae7772222bb23fc4448f9fb
Yari Andres <yari.andres@gmail.com>
2019-10-21 22:05:56 +0200

    adding new algo solved

--- algorithms.js
+++ algorithms.js
@@ -113,5 +113,15 @@
 //   }
 //   setTimeout(log, 300);
 // }
+// Rewrite the myConcat function which appends contents of arr2 to arr1 so that the function uses arrow function syntax.
+// var myConcat = function (arr1, arr2) {
+//   "use strict";
+//   return arr1.concat(arr2);
+// };
+// // test your code
+// console.log(myConcat([1, 2], [3, 4, 5]));
 
 
+const myConcat = (arr1, arr2) => arr1.concat(arr2)
+// test your code
+console.log(myConcat([1, 2], [3, 4, 5]));
